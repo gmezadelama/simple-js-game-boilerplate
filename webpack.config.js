f5c53e9760dde39d7fa351f0482da97ad67a5858
GUSTAVO ADOLFO MEZA DE LAMA <GUSTAVO@GUSTAVOs-MacBook-Pro.local>
2019-08-24 21:16:06 -0400

    Adding rules to webpack to handle images and fonts.

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,35 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                // Now we apply rule for images
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                      // Using file-loader for these files
+                      loader: "file-loader",
+       
+                      // In options we can set different things like format
+                      // and directory to save
+                      options: {
+                        outputPath: 'images'
+                      }
+                    }
+                ]
+            },
+            {
+                // Apply rule for fonts files
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [
+                    {
+                      // Using file-loader too
+                      loader: "file-loader",
+                      options: {
+                        outputPath: 'fonts'
+                      }
+                    }
+                ]
             }
         ]
     },
